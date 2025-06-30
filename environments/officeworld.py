dba4d4b5417a16a8490ad5f2c3b1d06537ab8611
Scarllette Ellis <re412@bath.ac.uk>
2025-06-30 14:28:25 +0100

    added skeleton methods to officeworld

--- environments/officeworld.py
+++ environments/officeworld.py
@@ -1,6 +1,6 @@
 import numpy as np
 import random as rand
-from typing import Tuple
+from typing import Any, List, Tuple, Type
 
 from environments.environment import Environment
 
@@ -193,6 +193,28 @@ class OfficeWorld(Environment):
         axis=0
     )
 
+    default_num_floors: int=3
+
+    x_locations = [2, 9, 14, 21]
+    y_locations = [2, 7, 12, 17, 22, 27]
+    default_goal_locations: List[Tuple[int, int, int]]=[
+        (y, x, floor)
+        for y in y_locations
+        for x in x_locations
+        for floor in range(default_num_floors)
+    ]
+
+    default_elevator_coord: Tuple[int, int, int]=(0, 5, 0)
+
+    default_start_coords: List[Tuple[int, int, int]]=[
+        (0, 17),
+        (0, 18),
+        (30, 5),
+        (30, 6),
+        (30, 17),
+        (30, 18)
+    ]
+
     invalid_action_reward = -1
     step_reward = -0.001
     success_reward = 2.0
@@ -200,10 +222,78 @@ class OfficeWorld(Environment):
     def __init__(
             self,
             floor_map: None|np.ndarray=None,
-            elevator_coord: None|Tuple[int]=None
+            elevator_coord: None|Tuple[int, int, int]=None,
+            start_coords: None|List[Tuple[int, int, int]]=None,
+            num_floors: None|int=None
     ):
-        self.state_dtype = None
-        self.state_shape = None
+        self.floor_map: np.ndarray = floor_map
+        if floor_map is None:
+            self.floor_map = self.default_floor_map
+        self.elevator_coord = elevator_coord
+        if elevator_coord is None:
+            self.elevator_coord = self.default_elevator_coord
+        self.start_coords = start_coords
+        if start_coords is None:
+            self.start_coords = self.default_start_coords
+        self.num_floors = num_floors
+        if num_floors is None:
+            self.num_floors = self.default_num_floors
+
+        self.state_dtype: Type = int
+        self.state_shape: Tuple[int, ...]=(self.floor_map.flatten().shape[0] + 1,)
+
+        self.current_floor: None|int=None
+        self.current_state: None|np.ndarray=None
+        self.terminal: bool=True
         return
 
+    def get_current_state(self) -> np.ndarray:
+        if self.terminal:
+            raise ValueError("Cannot get current state when environment is terminal")
+        return self.flatten_state(self.current_state, self.current_floor)
 
+    def get_transition_probability(
+            self,
+            state: np.ndarray,
+            action: int,
+            next_state: np.ndarray
+    ) -> float:
+        pass
+
+    def flatten_state(
+            self,
+            state: np.ndarray,
+            floor: int
+    ) -> np.ndarray:
+        flattened_state = np.zeros(self.state_shape, dtype=self.state_dtype)
+        flattened_state[:self.state_shape[0] - 1] = state.flatten()
+        flattened_state[self.state_shape[0]] = floor
+        return flattened_state
+
+    def get_start_states(self) -> List[np.ndarray]:
+        pass
+
+    def get_success_states(
+            self,
+            state: np.ndarray,
+    ) -> (List[np.ndarray], List[float]):
+        pass
+
+    def is_terminal(
+            self,
+            state: None|np.ndarray=None
+    ) -> bool:
+        pass
+
+    def step(
+            self,
+            action: int
+    ) -> (np.ndarray, float, bool, Any):
+        pass
+
+    def reset(
+            self,
+            start_state: None|np.ndarray=None,
+            seed: None|int=None
+    ) -> np.ndarray:
+        pass
