21ce2e140baa5e34920675302fb858980278160e
Scarllette Ellis <re412@bath.ac.uk>
2025-07-07 10:44:27 +0100

    added get_successor_state method

---
+++ environments/simpledeckbuilding.py
@@ -0,0 +1,10 @@
+from environments.environment import Environment
+
+# coin guards
+# victory point cards
+# action cards
+
+class SimpleDeckBuilding(Environment):
+
+    def __init__(self):
+        pass
